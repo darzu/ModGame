88285c611f30df4901db86deab4e7f520ae7706b
darzu <daryl.zuniga@gmail.com>
2019-08-27 16:49:35 -0700

    add custom block

--- main.ts
+++ main.ts
@@ -1,2 +1,5 @@
-//hello
-scene.setBackgroundColor(2)
+//% help=foobar/doStuff weight=100 blockNamespace="foobar"
+//% blockId=doStuff block="doStuff" blockAllowMultiple=1
+function doStuff(a: string): void {
+    scene.setBackgroundColor(2)
+}
